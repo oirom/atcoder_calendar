e9eab860074bb6871c41a5bb70fa9df37d99e92a
oirom <yamauchi.morio853@mail.kyutech.jp>
2020-05-19 18:00:15 +0900

    integrate functions that handle schedule data

--- get_schedule.py
+++ get_schedule.py
@@ -1,4 +1,4 @@
-import requests, re, bs4, json, os, shutil, datetime, urllib.request
+import requests, bs4, os, datetime
 from bs4 import BeautifulSoup
 from datetime import datetime as dt
 
@@ -9,33 +9,37 @@ def get_atcoder_schedule() :
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.content, "html.parser")
 
-    contest_date = []
+    start_date = []
+    end_date = []
     contest_name = []
 
     # <div id="contest-table-upcoming"> の内容を取得
     for s in soup.find_all('div', id="contest-table-upcoming") :
         # <time> の内容を取得してリストに格納
         for t in s.find_all('time') :
-            contest_date.append(t.contents[0])
+            start_date.append(t.contents[0])
         # <a href='/contests/~'> の内容を取得してリストに格納
         for c in s.select('a[href^="/contests"]') :
             contest_name.append(c.contents[0])
 
     # <time> から取得した時刻を datetime モジュールで計算可能な形に処理
-    contest_date = list(map(lambda x : str(dt.strptime(x, '%Y-%m-%d %H:%M:%S+0900')), contest_date))
+    start_date = list(map(lambda x : str(dt.strptime(x, '%Y-%m-%d %H:%M:%S+0900')), start_date))
 
-    return contest_name, contest_date
-
-if __name__ == '__main__':
-
-    name, start_date = get_atcoder_schedule()
-
-    print (name)
-    print (start_date)
-
-    # 取得した AtCoder の開始時刻から終了時刻を計算
+    # 取得した AtCoder の開始時刻をもとに終了時刻を計算
+    # 計算の際には日付の変更などに気を付ける必要あり
     end_date = list(map(lambda x : 
         str(dt.strptime(x, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(days=1, hours=1)) if dt.strptime(x, '%Y-%m-%d %H:%M:%S').hour==23 
         else str(dt.strptime(x, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=1)), start_date))
+    
+    # 日時データを google calender api で利用可能な形に整形（日付と時刻の間に T を入れるだけ）
+    start_date = list(map(lambda x : x.replace(" ", "T"), start_date))
+    end_date = list(map(lambda x : x.replace(" ", "T"), end_date))
 
-    print (end_date)
+    return contest_name, start_date, end_date
+
+if __name__ == '__main__':
+
+    names, start_dates, end_dates = get_atcoder_schedule()
+
+    for start, end, name in zip(start_dates, end_dates, names) :
+        print(start, end, name)
