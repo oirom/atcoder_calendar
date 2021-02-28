3178cf9e4ba3126f491c9f4b376e3d9d9e2c0fec
Keita Morisaki <kmta1236@gmail.com>
2021-02-28 23:13:14 +0900

    refactor

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 import requests, bs4
 import urllib.parse as urlparse
 from datetime import datetime as dt
-from typing import Final, List, Any, Dict
+from typing import Final, List, Any
 from dataclasses import dataclass
 
 
